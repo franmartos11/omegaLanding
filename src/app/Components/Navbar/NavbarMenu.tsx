179a892c17068429fcb2ebbb69ab277e39f07be7
franmartos11 <franmartos11@gmail.com>
2024-11-18 18:31:41 -0300

    third fix navbarmenu

--- src/app/Components/Navbar/NavbarMenu.tsx
+++ src/app/Components/Navbar/NavbarMenu.tsx
@@ -3,14 +3,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const transition = {
-  type: "spring",
-  mass: 0.5,
-  damping: 11.5,
-  stiffness: 100,
-  restDelta: 0.001,
-  restSpeed: 0.001,
-};
+
 
 export const Menu = ({
   
