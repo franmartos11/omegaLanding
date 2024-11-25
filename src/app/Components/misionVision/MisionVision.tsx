b89f4e07535b35c3526b3a94ff664c8a828420ac
franmartos11 <franmartos11@gmail.com>
2024-11-25 10:58:12 -0300

    add animations in view to component misionvision

--- src/app/Components/misionVision/MisionVision.tsx
+++ src/app/Components/misionVision/MisionVision.tsx
@@ -1,19 +1,30 @@
 "use client";
 
 import React from "react";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 const MissionVision = () => {
+  const [missionRef, missionInView] = useInView({ triggerOnce: true });
+  const [visionRef, visionInView] = useInView({ triggerOnce: true });
+
   return (
-    <section className=" py-16 px-6 relative text-center lg:text-left">
+    <section className="py-16 px-6 relative text-center lg:text-left">
       {/* Contenedor principal */}
       <div className="relative max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
         {/* Misión */}
-        <div className="relative z-10">
+        <motion.div
+          ref={missionRef}
+          initial={{ x: -100, opacity: 0 }}
+          animate={missionInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="relative z-10"
+        >
           <h2 className="text-3xl font-bold text-gray-800 uppercase mb-4">
             Nuestra Misión
           </h2>
           <div className="flex justify-center lg:justify-start mb-4">
-          <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
+            <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
             <span className="inline-block w-20 h-1 bg-orange-500 rounded-full"></span>
             <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
           </div>
@@ -22,30 +33,51 @@ const MissionVision = () => {
             ofreciéndoles todo lo que requieran sin importar el tipo de producto
             o servicio.
           </p>
-        </div>
+        </motion.div>
+
         {/* Imagen misión */}
-        <div className="relative flex justify-center lg:justify-end">
+        <motion.div
+          ref={missionRef}
+          initial={{ x: -100, opacity: 0 }}
+          animate={missionInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.5 }}
+          className="relative flex justify-center lg:justify-end"
+        >
           <img
             src="/nuestraMision.webp"
             alt="Misión"
             className="w-[22rem] h-auto object-cover rounded shadow-lg"
           />
-        </div>
+        </motion.div>
 
-        {/* Visión */}
-        <div className="relative flex justify-center lg:justify-start">
+        {/* Imagen visión */}
+        <motion.div
+          ref={visionRef}
+          initial={{ x: 100, opacity: 0 }}
+          animate={visionInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="relative flex justify-center lg:justify-start"
+        >
           <img
             src="/nuestraVision.webp"
             alt="Visión"
             className="w-[22rem] h-auto object-cover rounded shadow-lg"
           />
-        </div>
-        <div className="relative z-10">
+        </motion.div>
+
+        {/* Visión */}
+        <motion.div
+          ref={visionRef}
+          initial={{ x: 100, opacity: 0 }}
+          animate={visionInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
+          className="relative z-10"
+        >
           <h2 className="text-3xl font-bold text-gray-800 uppercase mb-4">
             Nuestra Visión
           </h2>
           <div className="flex justify-center lg:justify-start mb-4">
-          <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
+            <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
             <span className="inline-block w-20 h-1 bg-orange-500 rounded-full"></span>
             <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
           </div>
@@ -54,7 +86,7 @@ const MissionVision = () => {
             región y convertirnos así, en un aliado estratégico para su
             desarrollo.
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
