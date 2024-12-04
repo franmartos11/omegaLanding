1fd2c5b3bfb674c7c2a687d2739fed76d702a8f4
franmartos11 <franmartos11@gmail.com>
2024-12-04 16:55:49 -0300

    fix texts misionVidion

--- src/app/Components/misionVision/MisionVision.tsx
+++ src/app/Components/misionVision/MisionVision.tsx
@@ -20,7 +20,7 @@ const MissionVision = () => {
           transition={{ duration: 0.8, ease: "easeOut" }}
           className="relative z-10"
         >
-          <h2 className="text-4xl font-bold text-white uppercase mb-4">
+          <h2 className="text-3xl font-bold text-white uppercase mb-4">
             Nuestra Misión
           </h2>
           <div className="flex justify-center lg:justify-start mb-4">
@@ -28,7 +28,7 @@ const MissionVision = () => {
             <span className="inline-block w-20 h-1 bg-orange-500 rounded-full"></span>
             <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
           </div>
-          <p className="text-gray-100 text-2xl">
+          <p className="text-gray-100 text-xl">
             Solucionar los problemas de abastecimiento de nuestros clientes,
             ofreciéndoles todo lo que requieran sin importar el tipo de producto
             o servicio.
@@ -46,7 +46,7 @@ const MissionVision = () => {
           <img
             src="/nuestraMision.webp"
             alt="Misión"
-            className="w-[24rem] h-auto object-cover rounded shadow-lg"
+            className="w-[18rem] h-auto object-cover rounded shadow-lg"
           />
         </motion.div>
 
@@ -61,7 +61,7 @@ const MissionVision = () => {
           <img
             src="/nuestraVision.webp"
             alt="Visión"
-            className="w-[24rem] h-auto object-cover rounded shadow-lg"
+            className="w-[18rem] h-auto object-cover rounded shadow-lg"
           />
         </motion.div>
 
@@ -73,7 +73,7 @@ const MissionVision = () => {
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
           className="relative z-10"
         >
-          <h2 className="text-4xl font-bold text-white uppercase mb-4">
+          <h2 className="text-3xl font-bold text-white uppercase mb-4">
             Nuestra Visión
           </h2>
           <div className="flex justify-center lg:justify-start mb-4">
@@ -81,8 +81,8 @@ const MissionVision = () => {
             <span className="inline-block w-20 h-1 bg-orange-500 rounded-full"></span>
             <span className="inline-block w-10 h-1 mx-1 bg-orange-500 rounded-full"></span>
           </div>
-          <p className="text-gray-100 text-2xl">
-            Apuntamos a trabajar en conjunto con las empresas más grandes de la
+          <p className="text-gray-100 text-xl">
+            Trabajar en conjunto con las empresas más grandes de la
             región y convertirnos así, en un aliado estratégico para su
             desarrollo.
           </p>
